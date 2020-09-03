8f9062507ab21909e59e475320ccc483fc8db612
MKemp <mkemp@accu-time.com>
2020-09-03 16:08:59 -0400

    Make retrofit call to get user and then show email in viewmodel

--- app/src/main/java/com/mkemp/daggerpractice/network/auth/AuthApi.java
+++ app/src/main/java/com/mkemp/daggerpractice/network/auth/AuthApi.java
@@ -1,11 +1,13 @@
 package com.mkemp.daggerpractice.network.auth;
 
-import okhttp3.ResponseBody;
-import retrofit2.Call;
+import com.mkemp.daggerpractice.models.User;
+
+import io.reactivex.Flowable;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface AuthApi
 {
-    @GET
-    Call<ResponseBody> getFakeStuff();
+    @GET("users/{id}")
+    Flowable<User> getUser(@Path("id") int id);
 }
--- app/src/main/java/com/mkemp/daggerpractice/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/mkemp/daggerpractice/ui/auth/AuthViewModel.java
@@ -2,11 +2,15 @@ package com.mkemp.daggerpractice.ui.auth;
 
 import android.util.Log;
 
+import com.mkemp.daggerpractice.models.User;
 import com.mkemp.daggerpractice.network.auth.AuthApi;
 
 import javax.inject.Inject;
 
 import androidx.lifecycle.ViewModel;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel
 {
@@ -26,5 +30,34 @@ public class AuthViewModel extends ViewModel
         {
             Log.d(TAG, "AuthViewModel: auth API created successfully!");
         }
+        
+        authApi.getUser(1)
+                .toObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(Disposable d)
+                    {
+        
+                    }
+    
+                    @Override
+                    public void onNext(User user)
+                    {
+                        Log.d(TAG, "onNext: " + user.getEmail());
+                    }
+    
+                    @Override
+                    public void onError(Throwable e)
+                    {
+                        Log.e(TAG, "onError: ", e);
+                    }
+    
+                    @Override
+                    public void onComplete()
+                    {
+        
+                    }
+                });
     }
 }
